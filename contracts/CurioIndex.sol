dde57b5f3fd9031c082f3fd2480e7dbe259a8a1c
Travis <travisformayor@users.noreply.github.com>
2021-09-08 11:35:04 -0700

    consistent language

--- contracts/CurioIndex.sol
+++ contracts/CurioIndex.sol
@@ -13,68 +13,43 @@ ID 102: Unwrapped CRO2... etc
 ID 201: Wrapped CRO1
 ID 202: Wrapped CRO2... etc
 
-
-ID 301: 1-10 set
-ID 301: Phneep set
-ID 302: Cryptograffit set
-ID 303: other artist... etc
-
-ID 310: Full Set
-ID 311: 1-10 set
-
 // 0: story, 1: full, 2: phneep, 3: cryptograffiti, 4: cryptopop, 5: robek, 6: daniel friedman, 7: marisol vengas
 ID 301: Story
 ID 302: Full
 ID 303: Cryptograffiti
 ID 304: Phneep
 etc
-
-Collabland can then be programmed to look for IDs for a set by looking at ID 1, or 2, etc
-
-The new curio site can fetch all balances both wrapped and unwrapped in one call, by doing balanceOfBatch with IDs 101, 102...130, 201, 202...230
-
-if id between 101-131
-    use unwrapped contract
-if id between 201-231
-    use wrapped contract with id 1, 2, 3...
-if id > 300
-    use set mapping
 */
 
 contract CurioIndex is CommonConstants {
     function balanceOf(address _owner, uint256 _id) external view returns (uint256) {
-        console.log("calling balanceOf on curio wrapper for id %s", _id);
+        console.log("calling balanceOf for id %s", _id);
 
         uint256 bal;
-        uint256[] memory batchBal;
 
         if (_id < 200) {
-            require(_id > 100, "Invalid ID under 101");
-            require((_id - 100) < 31, "Invalid ID for unwrapped curio");
+            require(_id > 100, "Invalid ID: unwrapped id under 101");
+            require((_id - 100) < 31, "Invalid ID: unwrapped id over 130");
             // handle as unwrapped
-            // id is 101 and we want 0 (first array position), so subract 101
+            // if id is 101 and we want 0 (first array position), subract 101
             bal = unwrappedCards[_id - 101].balanceOf(_owner);
-            console.log("received balance from cro%s: %s", _id - 100, bal);
-        } else if (_id < 300) {
-            require((_id - 200) < 31, "Invalid ID for wrapped curio");
+            console.log("received cro%s balance: %s", _id - 100, bal);
+        } else if (_id > 200 && _id < 300) {
+            require((_id - 200) < 31, "Invalid ID: wrapped curio id over 30");
             // handle as wrapped
-            // id is 204 and we want 4, so subtract 200
+            // if id is 201 and we want 1 (wrapper token id), subract 200
             bal = curioWrapperContract.balanceOf(_owner, _id - 200);
-            console.log("received balance: %s", bal);
+            console.log("received wrapped balance: %s", bal);
         } else {
-            require((_id - 301) < 8, "Invalid ID for curio set");
+            require((_id - 301) < 8, "Invalid ID: curio set id over 308");
             // handle as wrapped set
-
+            // id is 301 and we want 0 (first array position), subract 301
             address[] memory ownerArray = new address[](cardSets[_id - 301].length);
             for (uint256 i = 0; i < cardSets[_id - 301].length; i++) {
                 ownerArray[i] = _owner;
             }
 
-            console.log("set cards length: %s", cardSets[_id - 301].length);
-            console.log("owner array length: %s", ownerArray.length);
-
-            batchBal = curioWrapperContract.balanceOfBatch(ownerArray, cardSets[_id - 301]);
-            console.log("recieved balances for story set: %s", batchBal[0]);
+            uint256[] memory batchBal = curioWrapperContract.balanceOfBatch(ownerArray, cardSets[_id - 301]);
 
             uint256 smallest = batchBal[0];
             for (uint256 i = 1; i < batchBal.length; i++) {
@@ -82,8 +57,8 @@ contract CurioIndex is CommonConstants {
                     smallest = batchBal[i];
                 }
             }
-
             bal = smallest;
+            console.log("recieved set %s balance: %s", _id, bal);
         }
 
         return bal;
